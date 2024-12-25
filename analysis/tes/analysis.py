7f1423ff4d17677ed58e5f6e2fed11b859d1251f
Nimish <nimish.ie19@nsut.ac.in>
2024-12-25 23:38:30 +0530

    parameter algo tested and verified

--- analysis/tes/analysis.py
+++ analysis/tes/analysis.py
@@ -7,7 +7,7 @@ class AnalysisFunc:
     1. smooth(df) to smooth global order data
     2. trans_state_time(df,dt) to calculate number of transitions, it's time and sync time for agiven simulation
     3. compute_params(df,dt) to calculate above parameters for n iterations
-    4. gives time units for X axis ticks to plot global order
+    4. get_time(df,dt) to get time units for X axis ticks to plot global order
 
     """
 
@@ -35,12 +35,17 @@ class AnalysisFunc:
             dt (float): Timestep taken for the simulation
 
         Returns:
-            dictionary: number of transitions 'num', Transition time 'trans_time', Avg Sync time 'state_time' if any
+            dictionary: 'num','trans_time','state_time','up_cros','low_cros'
         """
 
         data = np.array(df)
         trans_time = []
         state_time = []
+        up_cros = []
+        low_cros = []
+
+        data_indices = np.arange(len(data))
+        flag = 0
 
         while True:
 
@@ -58,24 +63,48 @@ class AnalysisFunc:
 
                 lower_crossing = low_upd[-1]
 
+                # updating crossings according to data_indices
+                if flag == 0:
+                    # storing crossings for plot
+                    up_cros.append(upper_crossing * dt)
+                    low_cros.append(lower_crossing * dt)
+
+                else:
+                    upd_upper_crossing = upper_crossing + data_indices[0]
+                    upd_lower_crossing = lower_crossing + data_indices[0]
+
+                    # storing crossings for plot
+                    up_cros.append(upd_upper_crossing * dt)
+                    low_cros.append(upd_lower_crossing * dt)
+
                 t = (upper_crossing - lower_crossing) * dt
                 trans_time.append(t)
-
                 # checking for another transition
-                fwd_data = np.array(data[upper_crossing:])
+                data = np.array(data[upper_crossing:])
+                data_indices = data_indices[upper_crossing:]
 
-                check = np.where(fwd_data <= 0.2)[0]
+                check = np.where(data <= 0.2)[0]
 
                 if check.size > 0:
-                    ind = np.where(fwd_data <= 0.1)[0]
+                    ind = np.where(data <= 0.1)[0]
                     if ind.size > 0:
 
                         lower_crs = ind[0]
-                        lower_crs_up = lower_crs + upper_crossing
+                        lower_crs_up = lower_crs + data_indices[0]
+
+                        if flag == 0:
+                            pass
+                        else:
+                            lower_crossing = upd_lower_crossing
                         st_time = (lower_crs_up - lower_crossing) * dt
+
+                        # storing crossing
+                        low_cros.append(lower_crs_up * dt)
                         state_time.append(st_time)
                         # creating data for other cycle of transition and state time
-                        data = np.array(data[lower_crs_up:])
+                        data = np.array(data[lower_crs:])
+                        data_indices = data_indices[lower_crs:]
+                        flag += 1
 
                     else:
 
@@ -94,8 +123,8 @@ class AnalysisFunc:
         if len(state_time) >= 1:
             state_time = sum(state_time) / len(state_time)
 
-        keys = ["num", "trans_time", "state_time"]
-        values = [num, trans_time, state_time]
+        keys = ["num", "trans_time", "state_time", "up_cros", "low_cros"]
+        values = [num, trans_time, state_time, up_cros, low_cros]
 
         trans_dict = dict(zip(keys, values))
         return trans_dict
@@ -121,6 +150,7 @@ class AnalysisFunc:
         itr_st = []
         itr_tran = []
         while i < df.shape[1]:
+            # print(i)
             df.columns = list(range(0, df.shape[1]))
             num_tran_sync = self.trans_state_time(self.smooth(df[i]), dt)
             num += num_tran_sync["num"]
@@ -134,20 +164,20 @@ class AnalysisFunc:
                 itr_st.append(i)
                 sync.append(num_tran_sync["state_time"])
             i += 1
-        sync_itr = dict(zip(itr_st, sync))
-        tran_itr = dict(zip(itr_tran, trans))
+        # sync_itr = itr_st
+        # tran_itr = itr_tran
         # print(trans)
         # print(sync)
         # print(num)
-        print(tran_per)
-        print(tran_itr)
-        print(sync_itr)
+        print(f"Number of transitions / 100 iteration {tran_per}")
+        print(f"Number of transition {itr_tran}")
+        print(f"Number of transition with more than one transition {itr_st}")
         keys = ["tran_per", "trans", "sync"]
         values = [tran_per, trans, sync]
         param_dict = dict(zip(keys, values))
         return param_dict
-    
-    def get_time(self,df,dt):
+
+    def get_time(self, df, dt):
         """Takes the global order dataframe, smooths it and gives time for X axis accordingly.
         Use it for plots.
 
@@ -157,9 +187,8 @@ class AnalysisFunc:
 
         Returns:
             list: time unit values for X axis
-        """        
-        df_smooth=self.smooth(df)
-        time_steps_na = list(range(0,df_smooth.shape[0])) 
-        time = np.multiply(time_steps_na,dt)
-        return [time]
-
+        """
+        df_smooth = self.smooth(df)
+        time_steps_na = list(range(0, df_smooth.shape[0]))
+        time = np.multiply(time_steps_na, dt)
+        return time
