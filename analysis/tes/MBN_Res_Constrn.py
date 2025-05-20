2e454968a89f140730b02e596e7e619be434e8c5
nimish <nimish.ie19@nsut.ac.in>
2025-05-20 08:17:37 +0530

    interim update

--- analysis/tes/MBN_Res_Constrn.py
+++ analysis/tes/MBN_Res_Constrn.py
@@ -64,10 +64,10 @@ class MBN_RC(tES_Adaptive, DataUtils):
         self.A = np.mat(nx.adjacency_matrix(self.G).todense())
         np.fill_diagonal(self.A, 0)
 
-        self.A[:,62]=0
-        self.A[:,275]=0
-        self.A[62,:]=0
-        self.A[275,:]=0
+        self.A[:,0]=0
+        self.A[:,213]=0
+        self.A[0,:]=0
+        self.A[213,:]=0
         
         #print("Average shortest path length: ", nx.average_shortest_path_length(self.G))
         #print("Avg clustering coefficient: ", nx.average_clustering(self.G))
